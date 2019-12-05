1a5df0195593569b21bb7d1826e2d9a0b5255a1c
Paweł <p.placzynski@gmail.com>
2019-12-05 10:01:05 +0100

    current progress day 4

--- src/pl/projectone/aoc/Main.java
+++ src/pl/projectone/aoc/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         //AdventDay1.calculateAoC3("./inputs/day1/input1.txt");
         //AdventDay2.calculateAoC2("./inputs/day2/input1.txt");
-        //AdventDay3.calculateAoC3("input5.txt");
+        //AdventDay3.calculateAoC3("./inputs/day3/input5.txt");
         AdventDay4.calculateAoC4(197487, 673251);
     }
 }
